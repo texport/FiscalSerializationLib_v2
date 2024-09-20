0f0f213190a69723aaa38dda8a51fd0ab8f2dcc8
Sergey Ivanov <ivanov.sergey.ekb@gmail.com>
2024-09-20 14:59:26 +0500

    Initial Commit

---
+++ FiscalSerializationLib_v2/FiscalSerializationLib_v2.h
@@ -0,0 +1,18 @@
+//
+//  FiscalSerializationLib_v2.h
+//  FiscalSerializationLib_v2
+//
+//  Created by Sergey Ivanov on 20.09.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FiscalSerializationLib_v2.
+FOUNDATION_EXPORT double FiscalSerializationLib_v2VersionNumber;
+
+//! Project version string for FiscalSerializationLib_v2.
+FOUNDATION_EXPORT const unsigned char FiscalSerializationLib_v2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FiscalSerializationLib_v2/PublicHeader.h>
+
+
---
+++ FiscalSerializationLib_v2Tests/FiscalSerializationLib_v2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  FiscalSerializationLib_v2Tests.swift
+//  FiscalSerializationLib_v2Tests
+//
+//  Created by Sergey Ivanov on 20.09.2024.
+//
+
+import Testing
+@testable import FiscalSerializationLib_v2
+
+struct FiscalSerializationLib_v2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
